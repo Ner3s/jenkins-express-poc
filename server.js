0e1b62c7d31b8a5f38fcf19ad2df448a97a7b80d
Alan Neres <alanneres10@hotmail.com>
2022-06-16 19:23:47 -0300

    feat: add dotenv lib

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const APP_NAME = process.env.APPLICATION_NAME || "Express App";
+const APP_NAME = process.env.APPLICATION_NAME || 'Express App';
 
 app.use(express.json());
 
@@ -9,7 +12,7 @@ app.get("/", (req, res, next) => {
   res.send({ "Hello World": "Olá Mundo" });
 });
 
-app.listen(PORT, () => {
+app.listen(`${PORT}`, () => {
   console.log(`Server is listening on port ${PORT}`);
   console.log(`Server run in BRANCH MAIN`);
   console.log(`APLICATION NAME = ${APP_NAME}`);
