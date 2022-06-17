3cf3af9988d808a466cc6d5d4ac697ecf20ab1c6
Alan Neres <alanneres10@hotmail.com>
2022-06-17 00:01:26 -0300

    chore: test with credentials

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const APP_NAME = process.env.APPLICATION_NAME || 'Express App';
+const APP_NAME = process.env.APPLICATION_NAME || "Express App";
+
+const packageJson = require("./package.json");
 
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
-  res.send({ "Hello World": "Olá Mundo" });
+  res.send({ "Hello World": "Olá Mundo", version: packageJson.version });
 });
 
 app.listen(`${PORT}`, () => {
